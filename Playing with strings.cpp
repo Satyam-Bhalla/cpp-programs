713aaf6aa3809ec7b41cd17335c8e7b86feaa088
Satyam-Bhalla <satyambhalla17@gmail.com>
2016-11-23 23:47:36 +0530

    Update Playing with strings.cpp

--- Playing with strings.cpp
+++ Playing with strings.cpp
@@ -1,3 +1,6 @@
+/*To Enter 2 strings from user and then shuffling 
+and adding 1 additional random character to it and then finding that 
+character and its position*/
 #include <algorithm>
 #include <iostream>
 #include <string>
@@ -6,26 +9,25 @@ using namespace std;
 
 int main()
 {
-    static const char alphanum[] =
-"ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*abcdefghijklmnopqrstuvwxyz1234567890";
+    static const char alphanum[] ="ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*abcdefghijklmnopqrstuvwxyz1234567890";
     system("cls");
     string str,shuffle;
     cout << "Please enter a  word: "; //ask for input
     cin >> shuffle;
-    str=shuffle;
-    random_shuffle(shuffle.begin(), shuffle.end());
-   // cout << shuffle << '\n';
+    str=shuffle;//copying shuffle value in str
+    random_shuffle(shuffle.begin(), shuffle.end()); //shuffling the string randomly
+  
     srand((unsigned)time(0));
     string i;
     i = alphanum[rand() % shuffle.length()];
-    shuffle +=i;
+    shuffle +=i;//adding the random character to string
     random_shuffle(shuffle.begin(),shuffle.end());
-    //cout<<shuffle;
+    //again shuffling
     cout << "The first string is ="<<str<<endl;
     cout << "After shuffling the sring ="<<shuffle<<endl;
-    cout<<i;
+    cout<<i;//added character
     int x;
     x = shuffle.find_first_of(i);
-    cout<<endl<<x+1;
+    cout<<endl<<x+1;//position of added character
     return 0;
 }
