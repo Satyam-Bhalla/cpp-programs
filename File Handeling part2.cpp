7b1d57d5b1f735063da7378df6059ec37347bb27
Satyam-Bhalla <satyambhalla17@gmail.com>
2016-11-26 23:09:02 +0530

    Add files via upload

---
+++ File Handeling part2.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<fstream>
+
+using namespace std;
+
+int main(){
+    //ofstream file("satyam.txt",ios :: app);
+    ofstream file("satyam.txt");
+    if(!file.is_open()){
+        cout<< "File not opened";
+    } else {
+    file << "Sanyam"<<endl;
+    file<< "is an idiot"<<endl;
+    file.close();
+    cout<< "Sucessfully written to the file";
+    }
+
+    return 0;
+
+}
