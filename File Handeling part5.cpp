ccf26dba77c9ba39e46b1c0b79a1c3412d56deaf
Satyam-Bhalla <satyambhalla17@gmail.com>
2016-11-26 23:33:26 +0530

    Update File Handeling part5.cpp

--- File Handeling part5.cpp
+++ File Handeling part5.cpp
@@ -1,30 +1,29 @@
 #include<iostream>
-#include<fstream>
+#include<fstream> // Header file for file operations
 using namespace std;
-
 int main(){
-    ifstream file("satyam.txt",ios::in);
-    if(!file.is_open()){
+    ifstream file("abc.txt",ios::in);//opening file in in mode
+    if(!file.is_open()){ //Checking file
         cout << "error";
     } else {
-    cout << file.tellg()<<endl;
+    cout << file.tellg()<<endl; //check the position of get pointer
     string line;
-    file.seekg(0);
-    getline(file,line);
+    file.seekg(0); //making the position of get pointer to 0 to read the file
+    getline(file,line); //copying data into line variable
     cout << line<<endl;
-    file.close();
+    file.close(); //closing file
     }
     /*####################################*/
-    ofstream myfile("satyam.txt",ios::out);
-    if(!myfile.is_open()){
+    ofstream myfile("abc.txt",ios::out); //creating ofstream object in out mode to write data
+    if(!myfile.is_open()){ //checking file
         cout << "error";
     } else {
-        cout << myfile.tellp()<<endl;
-        myfile << "C++ is good";
-        cout<< myfile.tellp()<<endl;
-        myfile.seekp(5);
+        cout << myfile.tellp()<<endl; //checking the position of put pointer
+        myfile << "C++ is good"; // writing into file
+        cout<< myfile.tellp()<<endl; //checking the put pointer
+        myfile.seekp(5); //Moving  the put pointer to the 5th position
         myfile << "c++ is awesomskdfjfbjsoe";
-        cout<< myfile.tellp()<<endl;
+        cout<< myfile.tellp()<<endl;//checking the put pointer
     }
 
     return 0;
