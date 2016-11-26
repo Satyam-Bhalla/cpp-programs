fc941bd0bcf1e7e5ce284ab1fce4eccab74c0ebf
Satyam-Bhalla <satyambhalla17@gmail.com>
2016-11-26 23:27:23 +0530

    Update File Handeling part4.cpp

--- File Handeling part4.cpp
+++ File Handeling part4.cpp
@@ -1,26 +1,25 @@
 #include<iostream>
-#include<fstream>
+#include<fstream>// Header file for file operations
 using namespace std;
-
 int main()
 {
-    fstream file("satyam.txt",ios::in | ios::out | ios::trunc);
+    fstream file("abc.txt",ios::in | ios::out | ios::trunc);//Opening the file in (in,out or trunc mode)
     if(!file.is_open()){
         cout<<"Error";
-    }else{
+    } else {
     cout << "file opened successfully"<<endl;
     cout << "Writing to the file"<<endl;
 
     file<< "C++ is awesome";
-    file.seekp(3);
-    file << "lkfsvlekkrjbfkerj";
+    file.seekp(3); //taking the put pointer to 3rd position for writing data into file
+    file << "abcdef";
     cout<<"reading from the file"<<endl;
-    file.seekg(0);
+    file.seekg(0); //Taking the get pointer to 0th position
     string line;
     while(file.good()){
-        getline(file,line);
+        getline(file,line); //copying data of file object into line variable
         cout<<"\n"<<line;
-    }
+            }
     }
     return 0;
 }
