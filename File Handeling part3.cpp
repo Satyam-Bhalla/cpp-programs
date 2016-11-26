a8f1045e6817c41a240fafe0dc250ffd477cfe30
Satyam-Bhalla <satyambhalla17@gmail.com>
2016-11-26 23:20:15 +0530

    Update File Handeling part3.cpp

--- File Handeling part3.cpp
+++ File Handeling part3.cpp
@@ -1,23 +1,18 @@
 #include<iostream>
-#include<fstream>
-
+#include<fstream> // Header file for file operations
 using namespace std;
-
 int main(){
-    ifstream file;
-    file.open("satyam.txt");
-    if(!file.is_open()){
+    ifstream file;//creating ifstream object for reading file
+    file.open("abc.txt");//opening file
+    if(!file.is_open()){        //checking file
         cout << "Error while opening the file";
     }else {
     cout << "Reading from the file"<<endl;
-    string line;
-    while(file.good()){
-    getline(file,line);
-    cout<< line << " ";
+    string line; //string type variable
+    while(file.good()){ //to check whether the operations on the file are not producing any error
+    getline(file,line); //copying the file object data into line variable
+    cout<< line << " "; //Displaying the result
     }
     }
-
-
-
     return 0;
 }
