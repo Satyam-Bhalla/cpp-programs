257cedcedbe85cf5f261f7559b087c08f21cc541
Satyam-Bhalla <satyambhalla17@gmail.com>
2016-11-26 23:14:07 +0530

    Update File Handeling part2.cpp

--- File Handeling part2.cpp
+++ File Handeling part2.cpp
@@ -1,20 +1,16 @@
 #include<iostream>
-#include<fstream>
-
+#include<fstream>//Header filefor file operations
 using namespace std;
-
 int main(){
-    
-    ofstream file("satyam.txt");
-    if(!file.is_open()){
+    ofstream file("abc.txt");//creating ofstream object and creating file abc.txt
+    if(!file.is_open()){                //Checking for file
         cout<< "File not opened";
     } else {
-    file << "Sanyam"<<endl;
-    file<< "is an idiot"<<endl;
-    file.close();
-    cout<< "Sucessfully written to the file";
+    file << "Computer"<<endl; //writing into the file using stream object file
+    file<< "is a dumb machine."<<endl;
+    file.close(); //closing the file
+    cout<< "Sucessfully written to the file"<<endl;
     }
-
     return 0;
 
 }
